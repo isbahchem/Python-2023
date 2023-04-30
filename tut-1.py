3a9b5824e833f6d7a0e8090342dc1ad537cab46f
Isbah <isbahmedferoz@gmail.com>
2023-04-30 20:08:04 +0500

    Tut-1 added

---
+++ tut-1.py
@@ -0,0 +1,8 @@
+print("This program is to test if conditions.")
+number=int(input("Please enter any digit: "))
+if(number>10):
+    print("Greater than 10")
+if(number>100):
+    print("Greater than 100")
+if(number>10000):
+    print("Greater than 10000")
