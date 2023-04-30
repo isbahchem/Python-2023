2d77db951e54cb5183f2cc257708ad7a47aec40b
Isbah <isbahmedferoz@gmail.com>
2023-04-30 19:48:09 +0500

    This repository is for learning and future reference.

---
+++ introduction.py
@@ -0,0 +1,3 @@
+'''Introduction to Python Development Environment. This is for reference, used in future python projects.'''
+print('''Introduction to Python Development Environment. This is for reference, used in future python projects.
+All the development in 2023 included in this repository.''')
