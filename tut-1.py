bbba2281ccec7d0817ed768791687aefbb62eac3
Isbah <isbahmedferoz@gmail.com>
2023-05-02 10:42:59 +0500

    tut-1.py indented using pycharm

--- tut-1.py
+++ tut-1.py
@@ -1,8 +1,8 @@
 print("This program is to test if conditions.")
-number=int(input("Please enter any digit: "))
-if(number>10):
+number = int(input("Please enter any digit: "))
+if number > 10:
     print("Greater than 10")
-if(number>100):
+if number > 100:
     print("Greater than 100")
-if(number>10000):
+if number > 10000:
     print("Greater than 10000")
