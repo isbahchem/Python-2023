4a754653b98aff59e7c6240dddc4a2b306712742
Isbah Ahmed Feroz <64509937+isbahchem@users.noreply.github.com>
2023-05-02 10:55:37 +0500

    Add files via upload

---
+++ python-comments.py
@@ -0,0 +1,5 @@
+# This is the way to write comments in python
+print("# is used to write comments in python")
+'''This is another way to write comments in python'''
+print(" \'\'\' is also used to write comments in python.")
+print("like this \'\'\' comment \'\'\' for multiline comments.")
