693dffe109913c110ce29d63cf53576d687ac237
Isbah <isbahmedferoz@gmail.com>
2023-05-03 10:10:18 +0500

    2 files added.

---
+++ data_types.py
@@ -0,0 +1,16 @@
+#!usr/bin/env python3
+
+print("Python has the following data types built-in by default, in these categories:")
+print('''Text Type:	str
+Numeric Types:	int, float, complex
+Sequence Types:	list, tuple, range
+Mapping Type:	dict
+Set Types:	set, frozenset
+Boolean Type:	bool
+Binary Types:	bytes, bytearray
+None Type:	NoneType''')
+print("Print the data type of the variable x:")
+print('''x = 5
+print(type(x))''')
+x = 5
+print(type(x))
---
+++ numbers.py
@@ -0,0 +1,17 @@
+#!usr/bin/env python3
+import random
+
+print('''There are three numeric types in Python:
+
+int
+float
+complex''')
+
+x = 1    # int
+y = 2.8  # float
+z = 1j   # complex
+print("int ", x)
+print("float ", y)
+print("complex ", z)
+print("Random numbers: ")
+print(random.randrange(1, 10))
